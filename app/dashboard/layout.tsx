93a99a9b343ce54627f8c2ad1a5ec783305b5726
Ana Ceaicovski <ceaicovski94@gmail.com>
2024-01-09 00:32:16 +0200

    implemented auth and metadata

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,10 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
 import { ReactNode } from 'react';
+import { Metadata } from 'next';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
